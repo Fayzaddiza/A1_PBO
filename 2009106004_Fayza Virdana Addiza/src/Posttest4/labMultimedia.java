796b5e0d0da72a1bdf5d7d5cbb57b43993e3197f
Fayzaddiza <fyzaddiza@gmail.com>
2022-04-19 23:33:52 +0800

    2009106004_Fayza Virdana Addiza_POSTTEST4

---
+++ 2009106004_Fayza Virdana Addiza/src/Posttest4/labMultimedia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Posttest4;
+
+/**
+ *
+ * @author Acer
+ */
+public class labMultimedia extends barangLab {
+    public labMultimedia(String namaPeminjam, String barang, String tempat, Long banyak, String kondisi) {
+        super(namaPeminjam, barang, tempat, banyak, kondisi);
+    }
+    
+    override
+        public
+}
